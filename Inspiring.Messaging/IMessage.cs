cb39cbc70aebf26a74e1d702fc4ee5490bd969e4
Daniel Berghold <microsoft@berghold.cc>
2021-01-18 23:55:02 +0100

    Added IMessage base interfaces.

--- Inspiring.Messaging/IMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Inspiring.Messaging {
-    public interface IMessage<out M, in R> where M : IMessage<M, R> {
-    }
-}
---
+++ Inspiring.Messaging/IMessage`2.cs
@@ -0,0 +1,9 @@
+﻿using Inspiring.Messaging.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inspiring.Messaging {
+    public interface IMessage<out M, in R> : IMessage<R> where M : IMessage<M, R> {
+    }
+}
