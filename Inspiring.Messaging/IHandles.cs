fb90abac89445d20733d47900c59d4e47baad59f
Daniel Berghold <microsoft@berghold.cc>
2021-01-18 21:21:03 +0100

    Added co/contravariance to interfaces.

--- Inspiring.Messaging/IHandles.cs
+++ Inspiring.Messaging/IHandles.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Inspiring.Messaging {
-    public interface IHandles<in M, R> where M : IMessage<M, R> {
+    public interface IHandles<in M, out R> where M : IMessage<M, R> {
         R Handle(M m);
     }
 }
