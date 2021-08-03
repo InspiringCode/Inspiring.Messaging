f948f26a40f4cbe7457aee2a3f1a7a70a203030e
Daniel Berghold <microsoft@berghold.cc>
2021-08-03 09:12:20 +0200

    Added DynamicSend test case.

--- Inspiring.Messaging.Tests/Messenger/MessengerTests.cs
+++ Inspiring.Messaging.Tests/Messenger/MessengerTests.cs
@@ -1,11 +1,12 @@
 ﻿using FluentAssertions;
 using NSubstitute;
+using System;
 using Xbehave;
 
 namespace Inspiring.Messaging.Tests.Messenger {
     public class MessengerTests : Feature {
         [Scenario]
-        internal void DynamicSendTests(IMessenger messenger, TestMessage msg, string expected, string result) {
+        internal void DynamicSendTests(IMessenger messenger, TestMessage msg, string expected, IComparable comparableResult, string result) {
             GIVEN["a mock messenger"] |= () => {
                 messenger = Substitute.For<IMessenger>();
                 messenger.Send(msg = new TestMessage()).Returns(expected = "TEST");
@@ -13,6 +14,9 @@ namespace Inspiring.Messaging.Tests.Messenger {
 
             WHEN["calling DynamicSend"] |= () => result = messenger.DynamicSend<string>(msg);
             THEN["the strongly typed Send is called"] |= () => result.Should().Be(expected);
+
+            WHEN["calling DynamicSend with a result base class or interface"] |= () => comparableResult = messenger.DynamicSend<IComparable>(msg);
+            THEN["the result is casted to the specified type/interface"] |= () => result.Should().Be(expected);
         }
 
         internal class TestMessage : IMessage<TestMessage, string> {
